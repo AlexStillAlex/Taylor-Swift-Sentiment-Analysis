9b0ac8c0bbb2c9c642803dd443dd192f9177a6fd
Alex <alexchudasama@lcpproperties.co.uk>
2024-08-22 16:43:54 +0100

    Gathering and cleaning data creating spotify class, testing features and preparing for future commits. I dont care if I expose my keys.

--- genius_lyrics.py
+++ genius_lyrics.py
@@ -8,7 +8,7 @@ import pandas as pd
 def clean_up(song_title):
 
     if "Ft" in song_title:
-        before_ft_pattern = re.compile(".*(?=\(Ft)")
+        before_ft_pattern = re.compile(r".*(?=\(Ft)")
         song_title_before_ft = before_ft_pattern.search(song_title).group(0)
         clean_song_title = song_title_before_ft.strip()
         clean_song_title = clean_song_title.replace("/", "-")
@@ -41,7 +41,7 @@ def get_all_songs_from_album(artist, album_name):
 def download_album_lyrics(artist, album_name): 
     
     # You will need to go to Genius Developers to get your own client access token
-    client_access_token = 'insert_your_client_access_token_here'
+    client_access_token = 'xTU8eX1Tw9-dBnXKM5_glqmamxXzGgpM4WPFMTFamelJOJkjPT8UJBQbFPUpwF3p'
     LyricsGenius = lyricsgenius.Genius(client_access_token)
     LyricsGenius.remove_section_headers = True
     
@@ -64,6 +64,38 @@ def download_album_lyrics(artist, album_name):
             Path(f"{artist_title}_{album_title}").mkdir(parents=True, exist_ok=True)
             
             song_object.save_lyrics(filename=custom_filename, extension='txt', sanitize=False)
+            
+            clean_download_lyrics(f"{custom_filename}.txt")
+            
         
         else:
             print('No lyrics')
+            
+            
+    # Downloaded lyrics contain extraneous first lines and characters in the final lines.
+def clean_download_lyrics(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    # Remove the first line
+    lines = lines[1:]
+    # Remove the last 8 characters matching the pattern 2 digits followed by 6 characters
+    if lines:
+        last_line = lines[-1]
+        last_line = last_line[:-7]
+        lines[-1] = last_line
+    
+    with open(file_path, 'w') as file:
+        file.writelines(lines)
+            
+# clean_download_lyrics("Taylor-Swift_Taylor-Swift/Teardrops-On-My-Guitar.txt")
+# Exampled
+# download_album_lyrics("Taylor Swift", "Taylor Swift")
+# download_album_lyrics("Taylor Swift", "Fearless")
+# download_album_lyrics("Taylor Swift", "Speak Now")
+# download_album_lyrics("Taylor Swift", "Red")
+# download_album_lyrics("Taylor Swift", "1989")
+# download_album_lyrics("Taylor Swift", "Reputation")
+# download_album_lyrics("Taylor Swift", "Lover")
+# download_album_lyrics("Taylor Swift", "folklore")
+download_album_lyrics("Taylor Swift", "evermore")
+download_album_lyrics("Taylor Swift", "Midnights")
---
+++ spotify_class.py
@@ -0,0 +1,56 @@
+import requests
+
+class SpotifyAPI:
+    def __init__(self, base_url,client_id,client_secret,access_token=None,):
+        self.base_url = base_url
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.access_token = access_token or self.get_access_token()
+    def get_access_token(self):
+        auth_url = 'https://accounts.spotify.com/api/token'
+        data = {
+        'grant_type': 'client_credentials',
+        'client_id': self.client_id,
+        'client_secret': self.client_secret  ,
+            }
+        auth_response = requests.post(auth_url, data=data)
+        print(auth_response.json())
+        return auth_response.json()['access_token']
+
+
+    def get_headers(self):
+        return {
+            'Authorization': f'Bearer {self.access_token}'
+        }
+
+    def get_track_features(self, track_id):
+        url = f'{self.base_url}/audio-features/{track_id}'
+        response = requests.get(url, headers=self.get_headers())
+        print(response.json())
+        return response.json()
+
+    def get_track_analysis(self, track_id):
+        url = f'{self.base_url}/audio-analysis/{track_id}'
+        response = requests.get(url, headers=self.get_headers())
+        print(response.json())
+        return response.json()
+    
+    def get_albums(self, artist_id):
+        url = f'{self.base_url}/artists/{artist_id}/albums'
+        response = requests.get(url, headers=self.get_headers())
+        print(response.json())
+        return response.json()
+    
+    def get_tracks(self,track_id):
+        url = f'{self.base_url}/albums/{track_id}/tracks'
+        response = requests.get(url, headers=self.get_headers())
+        print(response.json())
+        return response.json()
+
+if __name__ == '__main__':
+    base_url = 'https://api.spotify.com/v1'
+    spotify_api = SpotifyAPI(base_url=base_url,client_id='bfb11db6ff8345c6853009c506c16035',client_secret='2df933fe72084f7bb81d9624499931f6')
+    track_id = '6y0igZArWVi6Iz0rj35c1Y'
+    
+    spotify_api.get_track_features(track_id)
+    # spotify_api.get_track_analysis(track_id)
